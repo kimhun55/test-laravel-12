d3e44a3876270b958b731007dad7983a11fbeaf5
bass <kimhun55@gamil.com>
2025-11-30 13:02:35 +0700

    show img post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,6 +9,9 @@
                         <h3 class="mb-0">{{ $post->title }}</h3>
                     </div>
                     <div class="card-body">
+                        @if($post->img_path)
+                            <img src="{{ asset('storage/' . $post->img_path) }}" alt="Post Image" class="img-fluid mb-3" style="max-height: 300px; object-fit: cover;">
+                        @endif
                         <p>{{ $post->content }}</p>
                         <a href="{{ route('posts.index') }}" class="btn btn-secondary">Back to Posts</a>
                     </div>
